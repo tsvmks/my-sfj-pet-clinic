51c577f0a0a3c599e9a562cc26ee53495a1e5c50
tsvetkov <tsvetkov@krista.ru>
2020-11-29 20:52:13 +0300

    Create PetType Map Service Impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * @author tsvetkov
+ * @created 2020-11-29
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
