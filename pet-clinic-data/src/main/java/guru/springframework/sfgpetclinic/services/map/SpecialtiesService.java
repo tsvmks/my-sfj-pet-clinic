51c65981f6f849aa05aa6a1279817fff01603178
tsvetkov <tsvetkov@krista.ru>
2020-11-29 21:13:12 +0300

    Create Specialty Map Service Impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialtiesService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.CrudService;
+
+/**
+ * @author tsvetkov
+ * @created 2020-11-29
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
